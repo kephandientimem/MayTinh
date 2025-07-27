8d789c488ea3b3d771ae0a4728738c313b40ad6e
haophatneeeeeeeee <haop81891@gmail.com>
2025-07-27 22:07:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function appendToResult(value) {
 function calculate() {
   try {
     const result = eval(expression);
-    if (result === 8) {
+    if (result === 2) {
       document.getElementById('result').value = 'I Love You';
     } else {
       document.getElementById('result').value = result;
